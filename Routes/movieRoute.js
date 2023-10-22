f80bcf2b36f0b1371fb5eaa539d7bb9f841e152e
Saurabh Tiwari <saurabhtiwari9825@gmail.com>
2023-10-22 12:32:31 +0530

    liv

--- Routes/movieRoute.js
+++ Routes/movieRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require("../Controller/movieController")
 
-router.post("/booking", controller.storMovieBooking)
+router.post("/booking", controller.storeMovieBooking)
 router.get("/booking", controller.getMovieBooking)
 
 module.exports = router
